96121792102a261b27489f698d3822a036770b50
G-Leib <gleibovi@msudenver.edu>
2020-02-24 19:44:16 -0700

    initial commit

---
+++ FragmentPractice/app/src/main/java/com/example/fragmentpractice/MainActivity.kt
@@ -0,0 +1,209 @@
+package com.example.fragmentpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import androidx.fragment.app.Fragment
+import android.graphics.Color
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_frag_one.*
+import kotlinx.android.synthetic.main.fragment_frag_two.*
+import java.lang.Integer.max
+
+
+class MainActivity : AppCompatActivity() {
+
+    fun activateColorOne(state: Boolean) : Boolean {
+
+        if (state) {
+            supportFragmentManager.beginTransaction().hide(fragment1).commit()
+        } else {
+            supportFragmentManager.beginTransaction().show(fragment1).commit()
+        }
+
+        return !(state)
+    }
+
+    fun activateColorTwo(state: Boolean) : Boolean {
+
+        if (state) {
+            supportFragmentManager.beginTransaction().hide(fragment2).commit()
+        } else {
+            supportFragmentManager.beginTransaction().show(fragment2).commit()
+        }
+
+        return !(state)
+    }
+
+    fun getColor(state: Boolean, prog: Int) : Int {
+
+        var newColor : Int
+
+        if (state) {
+            newColor = prog
+        } else {
+            newColor = 0
+        }
+
+        return newColor
+    }
+
+    fun changeColor(stateOne: Boolean, stateTwo: Boolean, progOne: Int, progTwo: Int) : Int {
+
+        val newColOne = getColor(stateOne, progOne)
+        val newColTwo = getColor(stateTwo, progTwo)
+
+        val newCol = max(newColOne, newColTwo)
+
+        return newCol
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var colorOneActive = true
+        var colorTwoActive = true
+
+        var mainRed: Int
+        var mainGreen: Int
+        var mainBlue: Int
+
+        fun setFragThree() {
+            mainRed = changeColor(colorOneActive, colorTwoActive, sb_red_one.progress, sb_red_two.progress)
+            mainGreen = changeColor(colorOneActive, colorTwoActive, sb_green_one.progress, sb_green_two.progress)
+            mainBlue = changeColor(colorOneActive, colorTwoActive, sb_blue_one.progress, sb_blue_two.progress)
+
+            fragment3?.view?.setBackgroundColor(Color.rgb(mainRed, mainGreen, mainBlue))
+        }
+
+        col_one_btn.setOnClickListener {
+            colorOneActive = activateColorOne(colorOneActive)
+
+            setFragThree()
+        }
+
+        col_two_btn.setOnClickListener {
+            colorTwoActive = activateColorTwo(colorTwoActive)
+
+            setFragThree()
+        }
+
+        // Set reds
+        sb_red_one!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                fragment1?.view?.setBackgroundColor(Color.rgb(sb_red_one.progress, sb_green_one.progress, sb_blue_one.progress))
+
+                setFragThree()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+        sb_red_two!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                fragment2?.view?.setBackgroundColor(Color.rgb(sb_red_two.progress, sb_green_two.progress, sb_blue_two.progress))
+
+                setFragThree()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+        // Set greens
+        sb_green_one!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                fragment1?.view?.setBackgroundColor(Color.rgb(sb_red_one.progress, sb_green_one.progress, sb_blue_one.progress))
+
+                setFragThree()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+        sb_green_two!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                fragment2?.view?.setBackgroundColor(Color.rgb(sb_red_two.progress, sb_green_two.progress, sb_blue_two.progress))
+
+                setFragThree()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+        // Set blues
+        sb_blue_one!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                fragment1?.view?.setBackgroundColor(Color.rgb(sb_red_one.progress, sb_green_one.progress, sb_blue_one.progress))
+
+                setFragThree()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+        sb_blue_two!!.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Write code to perform some action when progress is changed.
+                fragment2?.view?.setBackgroundColor(Color.rgb(sb_red_two.progress, sb_green_two.progress, sb_blue_two.progress))
+
+                setFragThree()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is started.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Write code to perform some action when touch is stopped.
+            }
+        })
+
+
+
+    }
+
+
+}
